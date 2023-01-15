33cf739dc3de935e875672b3a30db7a6b61eff50
Edjek <edjek@hotmail.fr>
2023-01-15 02:40:49 +0100

    clean

--- 08_hover_me/app.js
+++ 08_hover_me/app.js
@@ -4,7 +4,6 @@ let back  = document.getElementById("back");
 button.addEventListener("mouseenter", function () {
 	back.style.marginRight = "24px";
 	back.style.marginBottom = "24px";
-	back.style.backgroundColor = "white";
 });
 button.addEventListener("mouseleave", function () {
 	back.style.marginLeft = "24px";
