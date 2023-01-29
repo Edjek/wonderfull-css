dcaf3c7d1cbb2face824a0a1ffa44b53a04ad13d
Edjek <edjek@hotmail.fr>
2023-01-29 19:24:58 +0100

    css animation

---
+++ 74_marmiton/js/app.js
@@ -0,0 +1,6 @@
+let wrap = document.querySelector(".wrap");
+let hamburger = document.querySelector(".hamburger");
+
+wrap.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+});
