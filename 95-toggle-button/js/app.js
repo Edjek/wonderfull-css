b9eb24d2c26abe820156688609bfdaf60e327f29
Edjek <edjek@hotmail.fr>
2023-08-16 08:06:04 +0200

    css

--- 95-toggle-button/js/app.js
+++ 95-toggle-button/js/app.js
@@ -1,8 +1,11 @@
 const body = document.querySelector('body')
 const toggle = document.querySelector('#toggle')
 const moon = document.querySelector('#moon')
+const checkbox = document.querySelector('#toggle input[type=checkbox]')
 
-toggle.addEventListener('click', () => {
+
+toggle.addEventListener('change', () => {
     moon.classList.toggle('active')
     body.classList.toggle('active')
+    console.log(checkbox.checked);
 })
