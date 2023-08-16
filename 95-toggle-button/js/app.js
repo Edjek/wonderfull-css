efe83e2e5aff56900d9b608ec4b3e9b4f3869240
Edjek <edjek@hotmail.fr>
2023-08-16 05:03:02 +0200

    css

--- 95-toggle-button/js/app.js
+++ 95-toggle-button/js/app.js
@@ -1,6 +1,8 @@
+const body = document.querySelector('body')
 const toggle = document.querySelector('#toggle')
 const moon = document.querySelector('#moon')
 
 toggle.addEventListener('click', () => {
     moon.classList.toggle('active')
+    body.classList.toggle('active')
 })
