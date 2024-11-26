f62c34ce3fd9726f159e45788e5157cfc08a82db
Edjek <edjek@hotmail.fr>
2024-11-26 19:39:10 +0100

    update

--- 50_expand_card/public/js/app.js
+++ 50_expand_card/public/js/app.js
@@ -2,8 +2,8 @@ const cards = document.querySelectorAll('.card');
 
 cards.forEach((card) => {
     card.addEventListener('click', () => {
-        cards.forEach((card) => {
-            card.classList.remove('active');
+        cards.forEach((c) => {
+            c.classList.remove('active');
         });
         card.classList.add('active');
     });
